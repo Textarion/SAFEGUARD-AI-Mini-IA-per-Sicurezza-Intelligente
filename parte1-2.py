94150f22c8480d4e0d8d3de0f86f56b6a5376318
Textarion <davidesferrazza217@gmail.com>
2026-04-09 11:10:03 +0200

    iuwfa
    qf9w8

--- parte1-2.py
+++ parte1-2.py
@@ -6,4 +6,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import RandomForestClassifier from sklearn.ensemble
-import train_test_split from
+import train_test_split from sklearn.model_selection
+import confusion_matrix from sklearn.metrics
+import accuracy_score from sklearn.metrics
+import classification_report from sklearn.metrics
